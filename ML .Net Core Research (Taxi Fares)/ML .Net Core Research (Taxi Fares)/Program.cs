65ad80028302f394dde662a6cf6d61ceb1221fed
justindd1994 <justindd1994@gmail.com>
2019-11-22 18:04:42 +0200

    Initial push.

---
+++ ML .Net Core Research (Taxi Fares)/ML .Net Core Research (Taxi Fares)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.ML;
+using ML__Net_Core_Research__Taxi_Fares_ML.Model;
+
+namespace ML_.Net_Core_Research__Taxi_Fares_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load the Model
+            MLContext mlContext = new MLContext();
+            ITransformer mlModel = mlContext.Model.Load("MLModel.zip", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            //Use the code below to add ionput data
+            ModelInput input = new ModelInput()// Create sample data
+            {
+                Vendor_id = "CMT",
+                Rate_code = 1,
+                Passenger_count = 1,
+                Trip_distance = 3.8f,
+                Payment_type = "CRD"
+            };
+
+            // Make prediction
+            ModelOutput prediction = ConsumeModel.Predict(input);
+
+            // Print Prediction
+            Console.WriteLine($"Predicted Fare: {prediction.Score}");
+            Console.ReadKey();
+        }
+    }
+}
